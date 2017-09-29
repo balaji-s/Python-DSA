4aa94d6cbd38ebb69588cc368a6ff44670d8dcf3
balaji-s <balajisundar.s@gmail.com>
2017-09-29 21:48:18 +0530

    committing myNode.py, cleaned up

--- LinkedList/myNode.py
+++ LinkedList/myNode.py
@@ -9,28 +9,26 @@ class Node:
     def __str__(self):
         return str(self.data)
 
-    def djs(self,node):
+    def printNode(self,node):
         if node.next == None:
             return
         print(node)
-        self.djs(node.next)
+        self.printNode(node.next)
         
 
-    def printNode(node):
-        while node:
-            print(node)
-            node = node.next
+    
   
 
 node1 = Node(2)
 node2 = Node(3)
 node3 = Node(4)
 node4 = Node(5)
+node5 = Node(6)
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
+node4.next = node5
 
 
-node1.djs(node1)
-print(node1, node2)
+node1.printNode(node1)
