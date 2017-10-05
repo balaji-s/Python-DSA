cee0534f6344c2b60f3e58df895fdd528d568877
Sundaram <balajisundar.s@gmail.com>
2017-10-05 16:13:50 +0530

    commiting node.py

--- LinkedList/Node.py
+++ LinkedList/Node.py
@@ -2,11 +2,7 @@ class Node(object):
 
     def __init__(self,data = None, next = None):
         self.data = data
-<<<<<<< HEAD
-        self.next = next
-=======
         self.node = None
->>>>>>> master
     
     def get_data(self):
         return self.data
@@ -17,23 +13,9 @@ class Node(object):
     def set_data(self, data):
         self.data = data
     
-<<<<<<< HEAD
-    def set_next(self, next):
-        self.next = next
-    def __str__(self):
-        return str(self.data)
-
-
-temp = Node("birni")
-temp1 = Node("irnib")
-temp.next = temp1
-print(temp)
-print(temp.get_data())
-=======
     def set_next(self, node):
         self.node = node
 
 
 
 
->>>>>>> master
