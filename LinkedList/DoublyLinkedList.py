0909ff306060a9a9e496cd0cc2311ddabac97535
Sundaram <balajisundar.s@gmail.com>
2017-10-10 19:34:44 +0530

    committing doublylinkedList.py

--- LinkedList/DoublyLinkedList.py
+++ LinkedList/DoublyLinkedList.py
@@ -1,7 +1,20 @@
 class doublyLinkedNode:
     def __init__(self, data):
         self.data = data
-        self.prev = None
-        self.next = None
+        self.prev_node = None
+        self.next_node = None
     
+    def set_next(self, next_node):
+        self.next_node = next_node
+    def set_prev(self, prev_node):
+        self.prev_node = prev_node
     
+    def get_data(self):
+        return self.data
+    def set_data(self, data):
+        self.data = data
+    
+    def get_next(self):
+        return self.next_node
+    def get_prev(self):
+        return self.prev_node
