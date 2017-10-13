4301b7a62b38e97b5ac3b50ca87431c3e2b27e34
Sundaram <balajisundar.s@gmail.com>
2017-10-13 18:27:53 +0530

    committing queue.py

--- Queue.py
+++ Queue.py
@@ -1,3 +1,6 @@
+from Array import Array
+
+
 class Queue:
 
     def __init__(self):
@@ -10,6 +13,7 @@ class Queue:
         self.queue_items.append(item)
     
     def dequeue(self):
+        assert not self.is_empty(), "cannot dequeue from empty queue"
         return self.queue_items.pop(0)
     
     def size(self):
@@ -19,11 +23,25 @@ class Queue:
 class circularQueue:
     def __init__(self , max_size):
 
-        self.cQueue = list()
         self.count = 0
         self.front = 0
         self.back = max_size - 1
-        
+        self.array = ;Array(max_size)
+    
+    def is_empty(self):
+        return self.array.size == 0
+
+    def is_full (self):
+        return self.count == len(self.array)
+    def __len__(self):
+        return self.count
+
+    def enqueue(self, item):
+        if !array.is_full():
+            self.array.
+
+
+
 '''myQue = Queue()
 print(myQue.is_empty())
 myQue.enqueue(2)
