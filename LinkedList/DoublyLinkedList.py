0e63e710c9fe0ecafee31aa282b6c4a6b447deec
balaji-s <balajisundar.s@gmail.com>
2017-10-18 17:37:59 +0530

    to be done insert, pop, index functions for doubly linked list

--- LinkedList/DoublyLinkedList.py
+++ LinkedList/DoublyLinkedList.py
@@ -44,18 +44,70 @@ class DLinkedList:
             prevnode.next_node = dnode
             self.tail.prev_node = dnode
         self.size += 1
-        
 
+    def find(self, item) :
+        found = False
+        stop = False
+        if self.head is None:
+            return found
+        else:
+            current = self.head
+        
+        while current is not None and not found:
+            if  item == current.data:
+                found = True
+            else:
+                current = current.next_node
+                found = False
+        return found
+
+    def pop(self, position = None):
+        if position is None:
+            nextnode = self.head.next_node
+            self.head = nextnode.next_node
+        elif position == self.size - 1:
+            prevnode = self.tail.prev_node
+            previous =  prevnode.prev_node
+            self.tail = previous
+        else:
+            if not self.is_empty():
+                current = self.head
+                while current is not None:
+                    pass
+        self.size -= 1
+
+    
+    def remove(self, item):
+        pass
+    
+    def insert(self, item, position):
+        pass
+    
+    def index(self, position):
+        pass
+    
     
     def traversal(self):
         current = self.head
-        while current is not None:
-            print(current.data)
+        while current :
+            if current.data is not None:
+                print(current.data)
             current = current.next_node
 
 dlinkedlist = DLinkedList()
 
-for i in range(5):
+'''for i in range(5):
     dlinkedlist.add_last(i)
 dlinkedlist.traversal()
-    
+print('------ traversal------')'''
+dkk = DLinkedList()
+for j in range(5):
+    dkk.add(j)
+#dkk.traversal()
+print(dkk.find(7))
+print(dkk.pop())
+print('--traversing after pop---')
+dkk.traversal()
+print('-traversing after removing last element using pop')
+dkk.pop(3)
+dkk.traversal()
