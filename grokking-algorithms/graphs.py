6dd54c12390f73cb3db155a7416204aa4fa35c08
balaji-s <balajisundar.s@gmail.com>
2018-03-11 21:21:08 +0530

    commiting graphs.py

--- grokking-algorithms/graphs.py
+++ grokking-algorithms/graphs.py
@@ -29,3 +29,60 @@ def  mango_seller(person):
     return "there is no mango seller"
 
 print(mango_seller(graph))
+
+'''
+dijkstra algorithm
+'''
+#first connect the dots using dictionary
+dgraph = {}
+dgraph['start'] ={}
+dgraph['start']['a'] = 6
+dgraph['start']['b'] = 2
+dgraph['a'] ={}
+dgraph['a']['finish'] = 1
+dgraph ['b'] ={}
+dgraph['b']['a'] = 3
+dgraph['b']['finish'] = 5
+dgraph['finish'] ={}
+
+#then calculate the costs from start to the next connecting points
+
+costs = {}
+infinity = float('inf')
+costs['a'] = 6
+costs['b'] = 2
+costs['finish'] = infinity
+
+#convert the parents of node into dictionary
+
+parents = {}
+parents ['a'] ='start'
+parents['b'] ='start'
+parents['finish'] =None
+
+processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = infinity
+    lowest_cost_node = None
+    for node in costs :
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not  None:
+    cost = costs[node]
+    neighbours = dgraph[node]
+    for n in neighbours.keys():
+        new_cost = cost + neighbours[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+print(node)
+
